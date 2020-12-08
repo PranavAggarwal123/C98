a9a21f5f6c24a2a1151aedbded73625123df7bcd
PranavAggarwal123 <70282598+PranavAggarwal123@users.noreply.github.com>
2020-12-07 19:49:05 -0500

    Add files via upload

---
+++ C98.py
@@ -0,0 +1,11 @@
+#f=open("Test.py")
+#f.read()
+#fileLines=f.readlines()
+#for line in fileLines
+#    print(line)
+introString  = "My Name Is Pranav. I Am 12 Years Old"
+words = introString.split(".")
+print(words)
+def greet(name):
+    print("Hello, "+ name + ". How Are You?")
+greet("Pranav")
